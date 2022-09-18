496bfce51be1c55f508b8d5bbb894775325277c0
Nikita Vasilev <nikitabmd2199@gmail.com>
2022-09-18 13:51:11 +0300

    9th commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import '../css/wa.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
