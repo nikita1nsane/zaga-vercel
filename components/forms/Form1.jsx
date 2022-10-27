28cff8560b4de2d4b81a0a30297538ec0bab6474
nikita1nsane <nikitabmd2199@gmail.com>
2022-10-27 10:00:08 +0300

    fix4

--- components/forms/Form1.jsx
+++ components/forms/Form1.jsx
@@ -67,70 +67,7 @@ const Form1 = (props) => {
 
   return (
     <>
-    <Script dangerouslySetInnerHTML={{
-    __html: `
-    /**
- * Getting and Transforming Form Data
- * @param {*} form
- * @returns data
- */
-let getData = (form) => {
-    const inputs = form.querySelectorAll('input');
-    let data = {
-        t_typ,
-        t_src,
-        t_mdm,
-        t_cmp,
-        t_cnt,
-        t_trm,
-        t_vst,
-        t_pgs,
-        t_afd,
-        t_cpg,
-        t_uag,
-        gid: getCookie_MW("_ga"),
-        yid: getCookie_MW("_ym_uid"),
-        from_page: window.location.href,
-    };
-    let key;
-    let val;
-
-    for (let i = 0; i < inputs.length; i++) {
-        key = inputs[i].getAttribute('name');
-        val = inputs[i].value;
-        data[key] = val;
-    }
-
-    return data;
-}
-let fetchData = (url, d) => {
-    fetch(url, {
-			method: "POST",
-			body: d,
-		})
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Success: ", data)
-        })
-        .catch((err) => {
-            console.log("Error: ", err);
-        });
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    let form = document.querySelectorAll('.forma');
-
-    for (let i = 0; i < form.length; i++) {
-        form[i].addEventListener('submit', (event) => {
-            event.preventDefault();
-            
-            let dataСollection = new FormData(event.target);
-            let sendData = fetchData("/site.app/bitrix24/b24Sender.php", dataСollection);
-        });
-    }
-});
-  `,
-  }} />
+    <Script src='./../../site.app/js/script.js' />
         <div className={props.classes}>
         <div className="close-form" onClick={() => func()}>
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
