4b3baff09c10f8ab004204a82cb44de5fcee7cb0
nikita1nsane <nikitabmd2199@gmail.com>
2022-11-01 00:01:49 +0300

    test

--- components/forms/Form1.jsx
+++ components/forms/Form1.jsx
@@ -101,54 +101,6 @@ const Form1 = (props) => {
         }
     }
 
-    /**
- * Getting and Transforming Form Data
- * @param {*} form
- * @returns data
- */
-let getData = (form) => {
-    const inputs = form.querySelectorAll('input');
-    let data = {
-        t_typ,
-        t_src,
-        t_mdm,
-        t_cmp,
-        t_cnt,
-        t_trm,
-        t_vst,
-        t_pgs,
-        t_afd,
-        t_cpg,
-        t_uag,
-        gid: getCookie_MW("_ga"),
-        yid: getCookie_MW("_ym_uid"),
-        from_page: window.location.href,
-    };
-    let key;
-    let val;
-
-    for (let i = 0; i < inputs.length; i++) {
-        key = inputs[i].getAttribute('name');
-        val = inputs[i].value;
-        data[key] = val;
-    }
-
-    return data;
-}
-let fetchData = (url, d) => {
-    fetch(url, {
-			method: "POST",
-			body: d,
-		})
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Success: ", data)
-        })
-        .catch((err) => {
-            console.log("Error: ", err);
-        });
-}
-
 
   return (
     <>
