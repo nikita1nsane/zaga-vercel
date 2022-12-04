ff6b3a9ac92d8f0ff0c20892015841302d178d8c
Nikita Vasilev <nikitabmd2199@gmail.com>
2022-12-04 14:07:21 +0300

    ffd

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../css/wa.css'
 import 'animate.css'
-import 'react-lazyload-youtube/dist/index.css'
 
 function MyApp({ Component, pageProps }) {
 
