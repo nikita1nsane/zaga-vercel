fbdeb55a8059d0ab67724734dfc5a939f16c44b8
Nikita Vasilev <nikitabmd2199@gmail.com>
2022-12-04 13:02:21 +0300

    team

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import img1 from '../images/team/1.webp'
+import img2 from '../images/team/2.webp'
+import img3 from '../images/team/3.webp'
+import Image from 'next/image'
 
 const Team = () => {
   return (
@@ -8,25 +12,25 @@ const Team = () => {
             <div className="h2">Наша команда</div>
             <div className='flex'>
                 <div className='flex-item'>
-                    <div className='team-img'></div>
+                    <Image src={img1} />
                     <div className='team-post'>Генеральный директор</div>
                     <div className='team-name'>Евгений Исаков</div>
                     <div className='team-mail' ><a href="mailto:isakov@zaga-game.com">isakov@zaga-game.com</a></div>
                 </div>
-                <div className='flex-item'>
+                {/* <div className='flex-item'>
                     <div className='team-img'></div>
                     <div className='team-post'>Технический директор</div>
                     <div className='team-name'>Федор Петров</div>
                     <div className='team-mail' ><a href="mailto:petrov@zaga-game.com">petrov@zaga-game.com</a></div>
-                </div>
+                </div> */}
                 <div className='flex-item'>
-                    <div className='team-img'></div>
+                    <Image src={img2} />
                     <div className='team-post'>Руководитель по развитию</div>
                     <div className='team-name'>Денис Фатеев</div>
                     <div className='team-mail' ><a href="mailto:sale@zaga-game.com">sale@zaga-game.com</a></div>
                 </div>
                 <div className='flex-item'>
-                    <div className='team-img'></div>
+                    <Image src={img3} />
                     <div className='team-post'>Маркетинг и партнёрство</div>
                     <div className='team-name'>Кирилл Николаев</div>
                     <div className='team-mail' ><a href="mailto:marketing@zaga-game.com">marketing@zaga-game.com</a></div>
